0492776cd5c83109d58d201dcd49b29f3b6894d8
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-22 20:43:50 -0400

    Added a menu repository, and menu list

---
+++ GraphQLProject/Services/MenuRepository.cs
@@ -0,0 +1,61 @@
+﻿using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+
+namespace GraphQLProject.Services
+{
+    public class MenuRepository : IMenuRepository
+    {
+        private static List<Menu> MenuList = new List<Menu>
+        {
+            new Menu
+            {
+                Id = 1,
+                Name = "Classic Burger",
+                Description = "A juicy Cheese Burger with lettuce and tomato",
+                Price = 10
+            },
+            new Menu
+            {
+                Id = 2,
+                Name = "Cheese Pizza",
+                Description = "A pizza with a garlic butter infused crust, sweet marinara sauce, and a melty mozzarella cheese blend",
+                Price = 15
+            },
+            new Menu
+            {
+                Id = 3,
+                Name = "Chicken Pasta Alfredo",
+                Description = "A fresh pasta with cheezy alfredo sauce and savory, juicy grilled chicken",
+                Price = 12
+            }
+        };
+
+        public Menu AddMenu(Menu menu)
+        {
+            MenuList.Add(menu);
+            return menu;
+        }
+
+        public void DeleteMenu(int id)
+        {
+            MenuList.RemoveAt(id);
+
+        }
+
+        public List<Menu> GetAllMenu()
+        {
+            return MenuList;
+        }
+
+        public Menu GetMenuById(int id)
+        {
+            return MenuList.Find(m => m.Id == id)!;
+        }
+
+        public Menu UpdateMenu(int id, Menu menu)
+        {
+            MenuList[id] = menu;
+            return
+        }
+    }
+}
