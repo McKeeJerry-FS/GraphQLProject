0bc01def7d749a958fe202c92f077af14c6c493d
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-22 21:10:18 -0400

    Added a couple GraphQL queries

---
+++ GraphQLProject/Query/MenuQuery.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Type;
+
+namespace GraphQLProject.Query
+{
+    public class MenuQuery : ObjectGraphType
+    {
+        public MenuQuery(IMenuRepository menuRepository)
+        {
+            Field<ListGraphType<MenuType>>("Menus").Resolve(context =>
+            {
+                return menuRepository.GetAllMenu();
+            });
+
+            Field<MenuType>("Menu").Arguments(new QueryArguments(new QueryArgument<InterfaceGraphType> { Name = "menuId"})).Resolve(context =>
+            {
+                return menuRepository.GetMenuById(context.GetArgument<int>("menuId"));
+            });
+
+        }
+    }
+}
