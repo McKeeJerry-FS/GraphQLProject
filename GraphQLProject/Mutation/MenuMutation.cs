07fbc6eff6baf82133bfaadbeaeb28248e9e255e
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-22 21:59:24 -0400

    Added mutations functions

---
+++ GraphQLProject/Mutation/MenuMutation.cs
@@ -0,0 +1,34 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+using GraphQLProject.Type;
+
+namespace GraphQLProject.Mutation
+{
+    public class MenuMutation : ObjectGraphType
+    {
+        public MenuMutation(IMenuRepository menuRepository) 
+        {
+            Field<MenuType>("CreateMenu").Arguments(new QueryArguments(new QueryArgument<MenuInputType> { Name = "menu" })).Resolve(context =>
+            {
+                return menuRepository.AddMenu(context.GetArgument<Menu>("menu"));
+            });
+
+            Field<MenuType>("UpdateMenu").Arguments(new QueryArguments(new QueryArgument<IntGraphType> { Name = "menuId" }, new QueryArgument<MenuInputType> { Name = "menu" })).Resolve(context =>
+            {
+                var menu = context.GetArgument<Menu>("menu");
+                var menuId = context.GetArgument<int>("menuId");
+                return menuRepository.UpdateMenu(menuId, menu);
+            });
+
+            Field<StringGraphType>("DeleteMenu").Arguments(new QueryArguments(new QueryArgument<IntGraphType> { Name = "menuId" })).Resolve(context =>
+            {
+                
+                var menuId = context.GetArgument<int>("menuId");
+                menuRepository.DeleteMenu(menuId);
+                return $"The menu against this id, {menuId} has been deleted";
+            });
+        }
+    }
+}
--- GraphQLProject/Program.cs
+++ GraphQLProject/Program.cs
@@ -1,5 +1,11 @@
+using GraphiQl;
+using GraphQL;
+using GraphQL.Types;
 using GraphQLProject.Interfaces;
+using GraphQLProject.Query;
+using GraphQLProject.Schema;
 using GraphQLProject.Services;
+using GraphQLProject.Type;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +14,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<IMenuRepository, MenuRepository>();
 
+builder.Services.AddTransient<MenuType>();
+builder.Services.AddTransient<MenuQuery>();
+builder.Services.AddTransient<ISchema, MenuSchema>();
+
+builder.Services.AddGraphQL(b => b.AddAutoSchema<ISchema>().AddSystemTextJson());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseGraphiQl("/graphql");
+app.UseGraphQL<ISchema>();
 
 app.UseAuthorization();
 
