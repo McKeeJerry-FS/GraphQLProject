2e586da1b120a46fae163dc5e16a6ef1fc458ff7
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-22 20:21:05 -0400

    Add project files.

---
+++ GraphQLProject/Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQLProject.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
